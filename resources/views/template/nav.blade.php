32e6eb5cf02f56ad84e5fbfd058c17c278dfb4dd
fajar ramdani <framdani220@gmail.com>
2022-07-26 12:06:55 +0700

    first commit

---
+++ resources/views/template/nav.blade.php
@@ -0,0 +1,17 @@
+
+    <nav class="navbar navbar-dark bg-dark">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a style="color: white" class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a style="color: white" class="nav-link" href="#">Login</a>
+            </li>
+            {{-- <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li> --}}
+          </ul>
+      </nav>
